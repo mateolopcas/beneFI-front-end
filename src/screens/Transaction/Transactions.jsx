0d3edbf5302b3a36f2040ecd4dca2e7831618d79
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-MacBook-Air.local>
2022-11-07 15:34:36 -0500

    transactions screen & component done

--- src/screens/Transaction/Transactions.jsx
+++ src/screens/Transaction/Transactions.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Transaction from '../components/Transaction.jsx'
 
-function Transactions() {
+function Transactions(props) {
+  props.user.transactions.sort((a, b) => b.date - a.date)
+
   return (
-    <div>Transactions</div>
+    <div className='transactions-screen'>
+      <h2>Transaction History:</h2>
+      <ul>
+        {props.user &&
+        props.user.transactions.map((transaction, index)=> {
+          return (
+              <Transaction
+                key={index}
+                data={transaction}
+              />
+          )
+        })
+        }
+      </ul>
+    </div>
   )
 }
 
