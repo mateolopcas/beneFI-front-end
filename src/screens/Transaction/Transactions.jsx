ad51a02a8b1cbec07906bc222a0c847d8df118f4
Mateo Lopez-Castillo <mateolopez-castillo@Mateos-MacBook-Air.local>
2022-11-08 11:09:52 -0500

    modifying transactions page

--- src/screens/Transaction/Transactions.jsx
+++ src/screens/Transaction/Transactions.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Transaction from '../../components/Transaction.jsx'
 
-function Transactions(props) {
-  props.user.transactions.sort((a, b) => b.date - a.date)
+function Transactions({user}) {
+  if (user.email.length > 0) {
+    user.transactions.sort((a, b) => b.date - a.date)
+  }
 
   return (
     <div className='transactions-screen'>
       <h2>Transaction History:</h2>
       <ul>
-        {props.user &&
-        props.user.transactions.map((transaction, index)=> {
+        {user.email.length > 0 &&
+        user.transactions.map((transaction, index)=> {
           return (
               <Transaction
                 key={index}
